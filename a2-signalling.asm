dbed635ff92e9e1d70cb481e370476825132f3b7
Cole Romick <159488011+crromick@users.noreply.github.com>
2025-12-27 16:30:05 -0800

    Add files via upload

---
+++ a2-signalling.asm
@@ -0,0 +1,583 @@
+; a2-signalling.asm
+; CSC 230: Fall 2022
+;
+; Student name: Cole Romick
+; Student ID: V01005730
+; Date of completed work:October 26th 2025
+;
+;http://www.rjhcoding.com/avr-asm-functions.php Help with understanding 
+;stacks and in avr
+;
+; *******************************
+; Code provided for Assignment #2
+;
+; Author: Mike Zastre (2022-Oct-15)
+;
+ 
+; This skeleton of an assembly-language program is provided to help you
+; begin with the programming tasks for A#2. As with A#1, there are "DO
+; NOT TOUCH" sections. You are *not* to modify the lines within these
+; sections. The only exceptions are for specific changes changes
+; announced on Brightspace or in written permission from the course
+; instructor. *** Unapproved changes could result in incorrect code
+; execution during assignment evaluation, along with an assignment grade
+; of zero. ****
+
+.include "m2560def.inc"
+.cseg
+.org 0
+
+; ***************************************************
+; **** BEGINNING OF FIRST "STUDENT CODE" SECTION ****
+; ***************************************************
+
+	; initializion code will need to appear in this
+    ; section
+
+ldi r20, 0xFF
+sts DDRL, r20
+out DDRB, r20
+
+clr r20
+
+ldi r20, LOW(RAMEND)
+ldi r21, HIGH(RAMEND)
+out SPL, r20
+out SPH, r21
+
+
+
+
+; ***************************************************
+; **** END OF FIRST "STUDENT CODE" SECTION **********
+; ***************************************************
+
+; ---------------------------------------------------
+; ---- TESTING SECTIONS OF THE CODE -----------------
+; ---- TO BE USED AS FUNCTIONS ARE COMPLETED. -------
+; ---------------------------------------------------
+; ---- YOU CAN SELECT WHICH TEST IS INVOKED ---------
+; ---- BY MODIFY THE rjmp INSTRUCTION BELOW. --------
+; -----------------------------------------------------
+
+	rjmp test_part_e
+	; Test code
+
+
+test_part_a:
+	ldi r16, 0b00100001
+	rcall set_leds
+	rcall delay_long
+
+	clr r16
+	rcall set_leds
+	rcall delay_long
+
+	ldi r16, 0b00111000
+	rcall set_leds
+	rcall delay_short
+
+	clr r16
+	rcall set_leds
+	rcall delay_long
+
+	ldi r16, 0b00100001
+	rcall set_leds
+	rcall delay_long
+
+	clr r16
+	rcall set_leds
+
+	rjmp end
+
+
+test_part_b:
+	ldi r17, 0b00101010
+	rcall slow_leds
+	ldi r17, 0b00010101
+	rcall slow_leds
+	ldi r17, 0b00101010
+	rcall slow_leds
+	ldi r17, 0b00010101
+	rcall slow_leds
+
+	rcall delay_long
+	rcall delay_long
+
+	ldi r17, 0b00101010
+	rcall fast_leds
+	ldi r17, 0b00010101
+	rcall fast_leds
+	ldi r17, 0b00101010
+	rcall fast_leds
+	ldi r17, 0b00010101
+	rcall fast_leds
+	ldi r17, 0b00101010
+	rcall fast_leds
+	ldi r17, 0b00010101
+	rcall fast_leds
+	ldi r17, 0b00101010
+	rcall fast_leds
+	ldi r17, 0b00010101
+	rcall fast_leds
+
+	rjmp end
+
+test_part_c:
+    ldi r16, 0b11111000
+    push r16
+    rcall leds_with_speed
+    pop r16
+
+    ldi r16, 0b11011100
+    push r16
+    rcall leds_with_speed
+    pop r16
+
+    ldi r20, 0b00100000
+test_part_c_loop:
+
+	clr r16
+    push r20
+    rcall leds_with_speed
+    pop r20
+    lsr r20
+    brne test_part_c_loop
+
+    rjmp end
+
+
+test_part_d:
+	ldi r21, 'E'
+	push r21
+	rcall encode_letter
+	pop r21
+	push r25
+	rcall leds_with_speed
+	pop r25
+
+	rcall delay_long
+
+	ldi r21, 'A'
+	push r21
+	rcall encode_letter
+	pop r21
+	push r25
+	rcall leds_with_speed
+	pop r25
+
+	rcall delay_long
+
+
+	ldi r21, 'M'
+	push r21
+	rcall encode_letter
+	pop r21
+	push r25
+	rcall leds_with_speed
+	pop r25
+
+	rcall delay_long
+
+	ldi r21, 'H'
+	push r21
+	rcall encode_letter
+	pop r21
+	push r25
+	rcall leds_with_speed
+	pop r25
+
+	rcall delay_long
+
+	rjmp end
+
+
+test_part_e:
+	ldi r25, HIGH(WORD02 << 1)
+	ldi r24, LOW(WORD02 << 1)
+	rcall display_message
+	rjmp end
+
+end:
+    rjmp end
+
+
+
+
+
+
+; ****************************************************
+; **** BEGINNING OF SECOND "STUDENT CODE" SECTION ****
+; ****************************************************
+
+set_leds:
+
+	push r16
+	push r18
+	push r19
+
+    clr r26 // PORTL LED register 
+    clr r27 // PORTB LED register
+    
+	; make r26 hold the significant bits for LED representation
+    sbrc r16, 0
+    ori  r26, 0b10000000  // for LED 6
+    sbrc r16, 1
+    ori  r26, 0b00100000    // for LED 5
+    sbrc r16, 2
+    ori  r26, 0b00001000   // for LED 4 
+    sbrc r16, 3
+    ori  r26, 0b00000010   // for LED 3
+
+    ; make r27 hold the significant bits for LED representation
+    sbrc r16, 4
+    ori  r27, 0b00001000    // for LED 2
+    sbrc r16, 5
+    ori  r27, 0b00000010    // for LED 1
+
+    ;out put the LEDs
+    sts PORTL, r26
+    out PORTB, r27
+
+	pop r19
+	pop r18
+	pop r16
+    ret
+
+
+slow_leds:
+	
+	push r16
+	push r17
+
+	; set LED and then delay appropriately
+	mov r16, r17
+	rcall set_leds
+	rcall delay_long
+	ldi r16, 0x00 // turn off LED
+	rcall set_leds
+	pop r17
+	pop r16
+	ret
+
+
+fast_leds:
+	push r16
+	push r17
+
+	; set LED and then delay appropriately
+	mov r16, r17
+	rcall set_leds
+	rcall delay_short
+	ldi r16, 0x00 // turn off LED
+	rcall set_leds
+	pop r17
+	pop r16
+	ret
+
+
+leds_with_speed:
+	push ZL
+	push ZH
+	
+	push r19
+	push r18
+
+	;set the stack pointers
+	in ZH, SPH
+	in ZL, SPL
+
+	;parameter from the stack onto temp
+	ldd r19, Z+8
+
+	mov r17, r19
+	andi r19, 0b11000000  
+
+	;branch for slow or fast 
+	cpi r19, 0b11000000 
+	breq slow
+
+	cpi r19, 0b00000000 
+	breq fast
+
+
+	rjmp wrapup1
+
+
+slow: 
+	
+	rcall slow_leds
+	rjmp wrapup1
+fast:
+	
+	rcall fast_leds
+	rjmp wrapup1
+
+
+
+
+
+wrapup1:
+	pop r18
+	pop r19
+	pop ZH
+	pop ZL
+	ret
+
+
+; Note -- this function will only ever be tested
+; with upper-case letters, but it is a good idea
+; to anticipate some errors when programming (i.e. by
+; accidentally putting in lower-case letters). Therefore
+; the loop does explicitly check if the hyphen/dash occurs,
+; in which case it terminates with a code not found
+; for any legal letter.
+
+encode_letter:
+	; protecting the registers to be used in the subroutine
+ 	push YH
+	push YL
+	push ZH
+	push ZL
+	push r18
+	push r22
+	push r26
+	push r24
+	push r23
+	push r19
+
+	;setting stack pointer
+	in YH, SPH
+	in YL, SPL
+
+	; getting adress pointer
+	ldi ZH, high(PATTERNS << 1)
+	ldi ZL, low(PATTERNS << 1)
+	clr r25
+	
+	
+	; loading registers
+	ldd r18,Y+14    
+	lpm r22, Z             
+	ldi r23, 0x2e  //"-" representation
+	ldi r24, 0x6f   // "o" representation
+	ldi r19, 0b00100000    
+                            
+	; will loop through PATTERNS table to find matching letter
+	matchLetter:
+		cp r18, r22
+		breq patternFind
+		adiw Z, 8           
+		lpm r22, Z         
+		rjmp matchLetter 
+
+	; loops and finds which LED's must be on to represent letter, and sorts by speed type 
+	patternFind:
+		cpi r19, 0x00      
+		breq checkSpeed       
+		adiw Z, 1           
+		lpm r22, Z         
+		cp r22, r24
+		breq setBit         
+		lsr r19
+		rjmp patternFind
+
+	;sets the bit to turn on according LED
+	setBit:
+		add r25, r19
+		lsr r19
+		rjmp patternFind
+
+	;finds proper speed of blink
+	checkSpeed:
+		adiw Z, 1           
+		lpm r22, Z         
+		cpi r22, 0x10      
+		breq duration       
+                            
+		rjmp wrapup2
+
+	; sets up the starting two bits to denote long/slow 
+	duration:
+		ldi r19, 0b11000000
+		add r25, r19
+
+	wrapup2:
+		pop r19
+		pop r23
+		pop r24
+		pop r26
+		pop r22
+		pop r18
+		pop ZL
+		pop ZH
+		pop YL
+		pop YH
+
+		ret 
+
+display_message:
+	push r25
+	push r24
+	push r23
+	push ZH
+	push ZL
+
+	; moves addresses
+	mov ZH, r25
+	mov ZL, r24
+	
+	; looping through the letters in the word
+	wordLoop:
+		lpm r23, Z+             ; loads first letter
+		tst r23                 ; checks for 0
+                                
+		breq wrapup3
+		push r23                
+		rcall encode_letter
+		pop r23
+		push r25                
+		rcall leds_with_speed   
+		pop r25                
+		rcall delay_long       ; delay for readability/ adhereing to given video
+
+		rjmp wordLoop 
+
+	wrapup3:
+		pop ZL
+		pop ZH
+		pop r23
+		pop r24
+		pop r25
+		ret
+
+
+
+; ****************************************************
+; **** END OF SECOND "STUDENT CODE" SECTION **********
+; ****************************************************
+
+
+
+
+; =============================================
+; ==== BEGINNING OF "DO NOT TOUCH" SECTION ====
+; =============================================
+
+; about one second
+delay_long:
+	push r16
+
+	ldi r16, 14
+delay_long_loop:
+	rcall delay
+	dec r16
+	brne delay_long_loop
+
+	pop r16
+	ret
+
+
+; about 0.25 of a second
+delay_short:
+	push r16
+
+	ldi r16, 4
+delay_short_loop:
+	rcall delay
+	dec r16
+	brne delay_short_loop
+
+	pop r16
+	ret
+
+; When wanting about a 1/5th of a second delay, all other
+; code must call this function
+;
+delay:
+	rcall delay_busywait
+	ret
+
+
+; This function is ONLY called from "delay", and
+; never directly from other code. Really this is
+; nothing other than a specially-tuned triply-nested
+; loop. It provides the delay it does by virtue of
+; running on a mega2560 processor.
+;
+delay_busywait:
+	push r16
+	push r17
+	push r18
+
+	ldi r16, 0x08
+delay_busywait_loop1:
+	dec r16
+	breq delay_busywait_exit
+
+	ldi r17, 0xff
+delay_busywait_loop2:
+	dec r17
+	breq delay_busywait_loop1
+
+	ldi r18, 0xff
+delay_busywait_loop3:
+	dec r18
+	breq delay_busywait_loop2
+	rjmp delay_busywait_loop3
+
+delay_busywait_exit:
+	pop r18
+	pop r17
+	pop r16
+	ret
+
+
+; Some tables
+
+PATTERNS:
+	; LED pattern shown from left to right: "." means off, "o" means
+    ; on, 1 means long/slow, while 2 means short/fast.
+	.db "A", "..oo..", 1
+	.db "B", ".o..o.", 2
+	.db "C", "o.o...", 1
+	.db "D", ".....o", 1
+	.db "E", "oooooo", 1
+	.db "F", ".oooo.", 2
+	.db "G", "oo..oo", 2
+	.db "H", "..oo..", 2
+	.db "I", ".o..o.", 1
+	.db "J", ".....o", 2
+	.db "K", "....oo", 2
+	.db "L", "o.o.o.", 1
+	.db "M", "oooooo", 2
+	.db "N", "oo....", 1
+	.db "O", ".oooo.", 1
+	.db "P", "o.oo.o", 1
+	.db "Q", "o.oo.o", 2
+	.db "R", "oo..oo", 1
+	.db "S", "....oo", 1
+	.db "T", "..oo..", 1
+	.db "U", "o.....", 1
+	.db "V", "o.o.o.", 2
+	.db "W", "o.o...", 2
+	.db "X", "oo....", 2
+	.db "Y", "..oo..", 2
+	.db "Z", "o.....", 2
+	.db "-", "o...oo", 1   ; Just in case!
+
+WORD00: .db "HELLOWORLD", 0, 0
+WORD01: .db "THE", 0
+WORD02: .db "QUICK", 0
+WORD03: .db "BROWN", 0
+WORD04: .db "FOX", 0
+WORD05: .db "JUMPED", 0, 0
+WORD06: .db "OVER", 0, 0
+WORD07: .db "THE", 0
+WORD08: .db "LAZY", 0, 0
+WORD09: .db "DOG", 0
+
+; =======================================
+; ==== END OF "DO NOT TOUCH" SECTION ====
+; =======================================
+
---
+++ a3part-D.asm
@@ -0,0 +1,746 @@
+;
+; a3part-D.asm
+;
+; Part D of assignment #3
+;
+;
+; Student name: Cole Romick
+; Student ID:V01005730
+; Date of completed work:DNF
+;
+; **********************************
+; Code provided for Assignment #3
+;
+; Author: Mike Zastre (2022-Nov-05)
+;
+; This skeleton of an assembly-language program is provided to help you 
+; begin with the programming tasks for A#3. As with A#2 and A#1, there are
+; "DO NOT TOUCH" sections. You are *not* to modify the lines within these
+; sections. The only exceptions are for specific changes announced on
+; Brightspace or in written permission from the course instruction.
+; *** Unapproved changes could result in incorrect code execution
+; during assignment evaluation, along with an assignment grade of zero. ***
+;
+
+
+; =============================================
+; ==== BEGINNING OF "DO NOT TOUCH" SECTION ====
+; =============================================
+;
+; In this "DO NOT TOUCH" section are:
+; 
+; (1) assembler direction setting up the interrupt-vector table
+;
+; (2) "includes" for the LCD display
+;
+; (3) some definitions of constants that may be used later in
+;     the program
+;
+; (4) code for initial setup of the Analog-to-Digital Converter
+;     (in the same manner in which it was set up for Lab #4)
+;
+; (5) Code for setting up three timers (timers 1, 3, and 4).
+;
+; After all this initial code, your own solutions's code may start
+;
+
+.cseg
+.org 0
+	jmp reset
+
+; Actual .org details for this an other interrupt vectors can be
+; obtained from main ATmega2560 data sheet
+;
+.org 0x22
+	jmp timer1
+
+; This included for completeness. Because timer3 is used to
+; drive updates of the LCD display, and because LCD routines
+; *cannot* be called from within an interrupt handler, we
+; will need to use a polling loop for timer3.
+;
+; .org 0x40
+;	jmp timer3
+
+.org 0x54
+	jmp timer4
+
+.include "m2560def.inc"
+.include "lcd.asm"
+
+.cseg
+#define CLOCK 16.0e6
+#define DELAY1 0.01
+#define DELAY3 0.1
+#define DELAY4 0.5
+
+#define BUTTON_RIGHT_MASK 0b00000001	
+#define BUTTON_UP_MASK    0b00000010
+#define BUTTON_DOWN_MASK  0b00000100
+#define BUTTON_LEFT_MASK  0b00001000
+
+#define BUTTON_RIGHT_ADC  0x032
+#define BUTTON_UP_ADC     0x0b0   ; was 0x0c3
+#define BUTTON_DOWN_ADC   0x160   ; was 0x17c
+#define BUTTON_LEFT_ADC   0x22b
+#define BUTTON_SELECT_ADC 0x316
+
+.equ PRESCALE_DIV=1024   ; w.r.t. clock, CS[2:0] = 0b101
+
+; TIMER1 is a 16-bit timer. If the Output Compare value is
+; larger than what can be stored in 16 bits, then either
+; the PRESCALE needs to be larger, or the DELAY has to be
+; shorter, or both.
+.equ TOP1=int(0.5+(CLOCK/PRESCALE_DIV*DELAY1))
+.if TOP1>65535
+.error "TOP1 is out of range"
+.endif
+
+; TIMER3 is a 16-bit timer. If the Output Compare value is
+; larger than what can be stored in 16 bits, then either
+; the PRESCALE needs to be larger, or the DELAY has to be
+; shorter, or both.
+.equ TOP3=int(0.5+(CLOCK/PRESCALE_DIV*DELAY3))
+.if TOP3>65535
+.error "TOP3 is out of range"
+.endif
+
+; TIMER4 is a 16-bit timer. If the Output Compare value is
+; larger than what can be stored in 16 bits, then either
+; the PRESCALE needs to be larger, or the DELAY has to be
+; shorter, or both.
+.equ TOP4=int(0.5+(CLOCK/PRESCALE_DIV*DELAY4))
+.if TOP4>65535
+.error "TOP4 is out of range"
+.endif
+
+reset:
+; ***************************************************
+; **** BEGINNING OF FIRST "STUDENT CODE" SECTION ****
+; ***************************************************
+
+; Anything that needs initialization before interrupts
+; start must be placed here.
+.def DATAH=r25  
+.def DATAL=r24
+.def BOUNDARY_H=r1
+.def BOUNDARY_L=r0
+.def BUTTON_CHAR_TEMP=r20
+
+.equ ADCSRA_BTN=0x7A
+.equ ADCSRB_BTN=0x7B
+.equ ADMUX_BTN=0x7C
+.equ ADCL_BTN=0x78
+.equ ADCH_BTN=0x79
+
+ldi r16, low(RAMEND)
+out SPL, r16
+;mov BOUNDARY_L, r21
+ldi r16, high(RAMEND)
+out SPH, r16
+;mov BOUNDARY_H, r21
+
+; ***************************************************
+; ******* END OF FIRST "STUDENT CODE" SECTION *******
+; ***************************************************
+
+; =============================================
+; ====  START OF "DO NOT TOUCH" SECTION    ====
+; =============================================
+
+	; initialize the ADC converter (which is needed
+	; to read buttons on shield). Note that we'll
+	; use the interrupt handler for timer 1 to
+	; read the buttons (i.e., every 10 ms)
+	;
+	ldi temp, (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0)
+	sts ADCSRA, temp
+	ldi temp, (1 << REFS0)
+	sts ADMUX, r16
+
+	; Timer 1 is for sampling the buttons at 10 ms intervals.
+	; We will use an interrupt handler for this timer.
+	ldi r17, high(TOP1)
+	ldi r16, low(TOP1)
+	sts OCR1AH, r17
+	sts OCR1AL, r16
+	clr r16
+	sts TCCR1A, r16
+	ldi r16, (1 << WGM12) | (1 << CS12) | (1 << CS10)
+	sts TCCR1B, r16
+	ldi r16, (1 << OCIE1A)
+	sts TIMSK1, r16
+
+	; Timer 3 is for updating the LCD display. We are
+	; *not* able to call LCD routines from within an 
+	; interrupt handler, so this timer must be used
+	; in a polling loop.
+	ldi r17, high(TOP3)
+	ldi r16, low(TOP3)
+	sts OCR3AH, r17
+	sts OCR3AL, r16
+	clr r16
+	sts TCCR3A, r16
+	ldi r16, (1 << WGM32) | (1 << CS32) | (1 << CS30)
+	sts TCCR3B, r16
+	; Notice that the code for enabling the Timer 3
+	; interrupt is missing at this point.
+
+	; Timer 4 is for updating the contents to be displayed
+	; on the top line of the LCD.
+	ldi r17, high(TOP4)
+	ldi r16, low(TOP4)
+	sts OCR4AH, r17
+	sts OCR4AL, r16
+	clr r16
+	sts TCCR4A, r16
+	ldi r16, (1 << WGM42) | (1 << CS42) | (1 << CS40)
+	sts TCCR4B, r16
+	ldi r16, (1 << OCIE4A)
+	sts TIMSK4, r16
+
+	sei
+
+; =============================================
+; ====    END OF "DO NOT TOUCH" SECTION    ====
+; =============================================
+
+; ****************************************************
+; **** BEGINNING OF SECOND "STUDENT CODE" SECTION ****
+; ****************************************************
+
+start:
+	rcall lcd_init
+	clr r16
+	sts CURRENT_CHAR_INDEX, r16
+	ldi r16, ' '
+	sts LAST_BUTTON_CHAR, r16
+	clr r16
+	sts BUTTON_WAS_PRESSED, r16
+	sts LAST_BUTTON_COL, r16
+	ldi r16, 0xFF
+	sts LAST_CLEARED_COL, r16
+
+	ldi r17, 16
+	ldi ZH, high(CURRENT_CHARSET_INDEX)
+	ldi ZL, low(CURRENT_CHARSET_INDEX)
+	clr r16
+
+init_charset_index:
+	st Z+, r16
+	dec r17
+	brne init_charset_index
+
+	ldi r17, 16
+	ldi ZH, high(TOP_LINE_CONTENT)
+	ldi ZL, low(TOP_LINE_CONTENT)
+	ldi r16, '0'
+
+init_top_line:
+	st Z+, r16
+	dec r17
+	brne init_top_line
+
+	;ldi r17, 16
+	;ldi ZH, high(CURRENT_CHARSET_INDEX)
+	;ldi ZL, low(CURRENT_CHARSET_INDEX)
+	;clr r16
+
+loop:
+
+timer3:
+	in r19, TIFR3
+	sbrs r19, OCF3A
+	rjmp timer3
+
+	ldi temp, 1<<OCF3A
+	out TIFR3, temp
+
+	ldi r18, 0
+	ldi ZH, high(TOP_LINE_CONTENT)
+	ldi ZL, low(TOP_LINE_CONTENT)
+
+display_top_line_loop:
+	ldi r16, 0
+	mov r17, r18
+	push r16
+	push r17
+	push r18
+	push ZH
+	push ZL
+	rcall lcd_gotoxy
+	pop ZL
+	pop ZH
+	pop r18
+	pop r17
+	pop r16
+
+	ld r16, Z+
+	push r16
+	push r18
+	push ZH
+	push ZL
+	rcall lcd_putchar
+	pop ZL
+	pop ZH
+	pop r18
+	pop r16
+
+	inc r18
+	cpi r18, 16
+	brne display_top_line_loop
+
+	lds r16, BUTTON_IS_PRESSED
+	cpi r16, 1
+	breq lcd_update_button
+	rjmp check_star_dash
+
+lcd_update_button:
+	lds r17, LAST_CLEARED_COL
+	cpi r17, 0xFF
+	breq DRAW_NEW_CHAR
+
+	ldi r16, 1
+	push r16
+	push r17
+	rcall lcd_gotoxy
+	pop r17
+	pop r16
+	
+	ldi r16, ' '
+	push r16
+	rcall lcd_putchar
+	pop r16
+
+DRAW_NEW_CHAR:
+	lds r17, LAST_BUTTON_COL
+	ldi r16, 1
+
+	push r16
+	push r17
+	rcall lcd_gotoxy
+	pop r17
+	pop r16
+
+	lds r16, LAST_BUTTON_CHAR
+	push r16
+	rcall lcd_putchar
+	pop r16
+
+	lds r17, LAST_BUTTON_COL
+	sts LAST_CLEARED_COL, R17
+
+	clr r16
+	sts BUTTON_WAS_PRESSED, r16
+
+check_star_dash:
+	lds r18, BUTTON_IS_PRESSED
+	cpi r18, 1
+	breq star
+
+dash:
+	ldi r16, 2
+	ldi r17, 15
+	push r16
+	push r17
+	rcall lcd_gotoxy
+	pop r17
+	pop r16
+
+	ldi r16, '-'
+	push r16
+	rcall lcd_putchar
+	pop r16
+
+	rjmp loop
+
+star:
+	ldi r16, 2
+	ldi r17, 15
+	push r16
+	push r17
+	rcall lcd_gotoxy
+	pop r17
+	pop r16
+
+	ldi r16, '*'
+	push r16
+	rcall lcd_putchar
+	pop r16
+		
+	rjmp loop
+
+timer1:
+	push r16
+	push r17
+	push r24
+	push BUTTON_CHAR_TEMP
+
+	lds r16, ADCSRA
+
+	ori r16, 0x40
+	sts ADCSRA, r16
+
+wait:
+	lds r16, ADCSRA
+	andi r16, 0x40
+	brne wait
+	lds DATAL, ADCL
+	lds DATAH, ADCH
+
+	ldi r16, low(BUTTON_SELECT_ADC)
+	mov BOUNDARY_L, r16
+	ldi r16, high(BUTTON_SELECT_ADC)
+	mov BOUNDARY_H, r16
+	cp DATAL, BOUNDARY_L
+	cpc DATAH, BOUNDARY_H
+	brlo CHECK_RIGHT
+	rjmp ButtonN
+
+CHECK_RIGHT:
+	ldi r16, low(BUTTON_RIGHT_ADC)
+	mov BOUNDARY_L, r16
+	ldi r16, high(BUTTON_RIGHT_ADC)
+	mov BOUNDARY_H, r16
+	cp DATAL, BOUNDARY_L
+	cpc DATAH, BOUNDARY_H
+	brsh CHECK_UP
+	rjmp ButtonR
+
+CHECK_UP:
+	ldi r16, low(BUTTON_UP_ADC)
+	mov BOUNDARY_L, r16
+	ldi r16, high(BUTTON_UP_ADC)
+	mov BOUNDARY_H, r16
+	cp DATAL, BOUNDARY_L
+	cpc DATAH, BOUNDARY_H
+	brsh CHECK_DOWN
+	rjmp ButtonU
+
+CHECK_DOWN:
+	ldi r16, low(BUTTON_DOWN_ADC)
+	mov BOUNDARY_L, r16
+	ldi r16, high(BUTTON_DOWN_ADC)
+	mov BOUNDARY_H, r16
+	cp DATAL, BOUNDARY_L
+	cpc DATAH, BOUNDARY_H
+	brsh CHECK_LEFT
+	rjmp ButtonD
+
+CHECK_LEFT:
+	ldi r16, low(BUTTON_LEFT_ADC)
+	mov BOUNDARY_L, r16
+	ldi r16, high(BUTTON_LEFT_ADC)
+	mov BOUNDARY_H, r16
+	cp DATAL, BOUNDARY_L
+	cpc DATAH, BOUNDARY_H
+	brsh ButtonN_jump
+	rjmp ButtonL
+
+ButtonN_jump:
+	rjmp ButtonN
+
+ButtonR:
+	ldi r17, 1
+	sts BUTTON_IS_PRESSED, r17
+	ldi r17, 1
+	sts LAST_BUTTON_PRESSED, r17
+	ldi BUTTON_CHAR_TEMP, 'R'
+	ldi r17, 3
+	sts LAST_BUTTON_COL, r17
+	rjmp UPDATE_CHAR
+
+ButtonU:
+	ldi r17, 1
+	sts BUTTON_IS_PRESSED, r17
+	ldi r17, 4
+	sts LAST_BUTTON_PRESSED, r17
+	ldi BUTTON_CHAR_TEMP, 'U'
+	ldi r17, 2
+	sts LAST_BUTTON_COL, r17
+	rjmp UPDATE_CHAR
+
+ButtonD:
+	ldi r17, 1
+	sts BUTTON_IS_PRESSED, r17
+	ldi r17, 3
+	sts LAST_BUTTON_PRESSED, r17
+	ldi BUTTON_CHAR_TEMP, 'D'
+	ldi r17, 1
+	sts LAST_BUTTON_COL, r17
+	rjmp UPDATE_CHAR
+
+ButtonL:
+	ldi r17, 1
+	sts BUTTON_IS_PRESSED, r17
+	ldi r17, 2
+	sts LAST_BUTTON_PRESSED, r17
+	ldi BUTTON_CHAR_TEMP, 'L'
+	ldi r17, 0
+	sts LAST_BUTTON_COL, r17
+	rjmp UPDATE_CHAR
+
+UPDATE_CHAR:
+	sts LAST_BUTTON_CHAR, BUTTON_CHAR_TEMP
+
+	ldi r17, 1
+	sts BUTTON_WAS_PRESSED, r17
+	rjmp end
+
+ButtonN:
+	ldi r17, 0
+	sts BUTTON_IS_PRESSED, r17
+
+end:
+	pop BUTTON_CHAR_TEMP
+	pop r24
+	pop r17
+	pop r16
+	reti
+
+timer4:
+	push r16
+	push r17
+	push r18
+	push r19
+	push ZH
+	push ZL
+
+	ldi r16, (1 << OCF4A)
+	sts TIFR4, r16
+
+	lds r16, BUTTON_IS_PRESSED
+	cpi r16, 1
+	breq button_pressed
+	rjmp timer4_end
+
+button_pressed:
+	lds r16, LAST_BUTTON_PRESSED
+	cpi r16, 4
+	brne check_down_btn
+	rjmp increment_charset
+
+check_down_btn:
+	cpi r16, 3
+	brne check_right_btn
+	rjmp decrement_charset
+check_right_btn:
+	cpi r16, 1
+	brne check_left_btn
+	rjmp move_right
+check_left_btn:
+	cpi r16, 2
+	brne skip_to_end
+	rjmp move_left
+
+skip_to_end:
+	rjmp timer4_end
+
+move_right:
+	lds r16, CURRENT_CHAR_INDEX
+	inc r16
+	cpi r16, 16
+	brlo update_display_after_move
+	clr r16
+	rjmp update_display_after_move
+
+move_left:
+	lds r16, CURRENT_CHAR_INDEX
+	tst r16
+	breq wrap_to_15
+	dec r16
+	rjmp update_display_after_move
+
+wrap_to_15:
+	ldi r16, 15
+
+update_display_after_move:
+	sts CURRENT_CHAR_INDEX, r16
+	mov r19, r16
+	rjmp timer4_end
+
+
+increment_charset:
+	lds r19, CURRENT_CHAR_INDEX
+
+	ldi ZH, high(CURRENT_CHARSET_INDEX)
+	ldi ZL, low(CURRENT_CHARSET_INDEX)
+	add ZL, r19
+	clr r18
+	adc ZH, r18
+	ld r16, Z
+
+	push ZH
+	push ZL
+	ldi ZH, high(AVAILABLE_CHARSET << 1)
+	ldi ZL, low(AVAILABLE_CHARSET << 1)
+	clr r17
+
+find_length_inc:
+	lpm r18, Z+
+	cpi r18, 0
+	breq found_length_inc
+	inc r17
+	rjmp find_length_inc
+
+found_length_inc:
+	pop ZL
+	pop ZH
+
+	inc r16
+	cp r16, r17
+	brlo store_charset_index
+	clr r16
+	rjmp store_charset_index
+
+decrement_charset:
+	lds r19, CURRENT_CHAR_INDEX
+
+	ldi ZH, high(CURRENT_CHARSET_INDEX)
+	ldi ZL, low(CURRENT_CHARSET_INDEX)
+	add ZL, r19
+	clr r18
+	adc ZH, r18
+	ld r16, Z
+
+	push ZH
+	push ZL
+	ldi ZH, high(AVAILABLE_CHARSET << 1)
+	ldi ZL, low(AVAILABLE_CHARSET << 1)
+	clr r17
+
+find_length_dec:
+	lpm r18, Z+
+	cpi r18, 0
+	breq found_length_dec
+	inc r17
+	rjmp find_length_dec
+
+found_length_dec:
+	pop ZL
+	pop ZH
+
+	tst r16
+	breq wrap_charset_end
+	dec r16
+	rjmp store_charset_index
+
+wrap_charset_end:
+	dec r17
+	mov r16, r17
+
+store_charset_index:
+	ldi ZH, high(CURRENT_CHARSET_INDEX)
+	ldi ZL, low(CURRENT_CHARSET_INDEX)
+	add ZL, r19
+	clr r18
+	adc ZH, r18
+	st Z, r16
+
+	ldi ZH, high(AVAILABLE_CHARSET << 1)
+	ldi ZL, low(AVAILABLE_CHARSET << 1)
+	add ZL, r16
+	clr r18
+	adc ZH, r18
+	lpm r16, Z
+
+	ldi ZH, high(TOP_LINE_CONTENT)
+	ldi ZL, low(TOP_LINE_CONTENT)
+	add ZL, r19
+	clr r18
+	adc ZH, r18
+	st Z, r16
+
+timer4_end:
+	pop ZL
+	pop ZH
+	pop r19
+	pop r18
+	pop r17
+	pop r16
+	reti
+
+; ****************************************************
+; ******* END OF SECOND "STUDENT CODE" SECTION *******
+; ****************************************************
+
+
+; =============================================
+; ==== BEGINNING OF "DO NOT TOUCH" SECTION ====
+; =============================================
+
+; r17:r16 -- word 1
+; r19:r18 -- word 2
+; word 1 < word 2? return -1 in r25
+; word 1 > word 2? return 1 in r25
+; word 1 == word 2? return 0 in r25
+;
+compare_words:
+	; if high bytes are different, look at lower bytes
+	cp r17, r19
+	breq compare_words_lower_byte
+
+	; since high bytes are different, use these to
+	; determine result
+	;
+	; if C is set from previous cp, it means r17 < r19
+	; 
+	; preload r25 with 1 with the assume r17 > r19
+	ldi r25, 1
+	brcs compare_words_is_less_than
+	rjmp compare_words_exit
+
+compare_words_is_less_than:
+	ldi r25, -1
+	rjmp compare_words_exit
+
+compare_words_lower_byte:
+	clr r25
+	cp r16, r18
+	breq compare_words_exit
+
+	ldi r25, 1
+	brcs compare_words_is_less_than  ; re-use what we already wrote...
+
+compare_words_exit:
+	ret
+
+.cseg
+AVAILABLE_CHARSET: .db "0123456789abcdef_", 0
+
+
+.dseg
+
+BUTTON_IS_PRESSED: .byte 1			; updated by timer1 interrupt, used by LCD update loop
+LAST_BUTTON_PRESSED: .byte 1        ; updated by timer1 interrupt, used by LCD update loop
+
+TOP_LINE_CONTENT: .byte 16			; updated by timer4 interrupt, used by LCD update loop
+CURRENT_CHARSET_INDEX: .byte 16		; updated by timer4 interrupt, used by LCD update loop
+CURRENT_CHAR_INDEX: .byte 1			; ; updated by timer4 interrupt, used by LCD update loop
+
+
+; =============================================
+; ======= END OF "DO NOT TOUCH" SECTION =======
+; =============================================
+
+
+; ***************************************************
+; **** BEGINNING OF THIRD "STUDENT CODE" SECTION ****
+; ***************************************************
+
+.dseg
+LAST_BUTTON_CHAR: .byte 1
+LAST_BUTTON_COL: .byte 1
+BUTTON_WAS_PRESSED: .byte 1
+LAST_CLEARED_COL: .byte 1
+
+
+; If you should need additional memory for storage of state,
+; then place it within the section. However, the items here
+; must not be simply a way to replace or ignore the memory
+; locations provided up above.
+
+
+; ***************************************************
+; ******* END OF THIRD "STUDENT CODE" SECTION *******
+; ***************************************************
